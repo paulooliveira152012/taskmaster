0caa2dee9739c2e229317ad4449a4bb8599696be
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-04-08 18:22:22 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,16 +81,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
-    console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
-    console.log(ui);
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log(event);
+    $(this).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out: function(event) {
-    console.log(event);
+    $(this).removeClass("dropover-active")
+    $(".bottom-trash").removeClass("bottom-trash-active")
   },
   update: function() {
     var tempArr = [];
@@ -161,7 +165,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -288,3 +292,11 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+
+// setting a function to run every X ammount of millisecond 
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
